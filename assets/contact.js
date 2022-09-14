76d84dc6dcf858ebf6ca2536087b31dda28e18e6
alelof <alejandra.lof@gmail.com>
2022-09-14 15:00:31 +0200

    v0.3

--- assets/contact.js
+++ assets/contact.js
@@ -7,12 +7,84 @@ Si no es válido, mostramos un mensaje de error. En ningún caso el formulario d
 */
 document.getElementById('submitBtn').addEventListener('click', 
 function(e){
-  validateFields();
+  //validateFields();
   e.preventDefault();
+  const nam = document.getElementById('name').value;
+  const mai = document.getElementById('email').value;
+  const pho = document.getElementById('phone').value;
+  const msg = document.getElementById('message').value;
+  const missingData = document.getElementById('missingData');
+  const wrongPhone = document.getElementById('wrongPhone');
+  const wrongMsg = document.getElementById('wrongMsg');
+  const allOk = document.getElementById('allOk');
+  let proceed = true;
+//console.log(nam+mai+pho+msg);
+
+  if (!nam || !mai || !pho || !msg){ //EmptyFields
+      proceed = false;
+      missingData.style.display = 'block';
+      missingData.scrollIntoView();
+      setTimeout(() => {
+        missingData.style.display = 'none';
+      }, 4000);
+      //console.log("vacios:"+nam+mai+pho+msg);      
+  }
+  if(pho[0] != '+'){
+      proceed = false;
+      wrongPhone.style.display = 'block';
+      wrongPhone.scrollIntoView();  
+      setTimeout(() => {
+        wrongPhone.style.display = 'none';
+      }, 4000);
+  }
+  if(msg.length > 50){
+    proceed = false;
+    wrongMsg.style.display = 'block';
+    wrongMsg.scrollIntoView();
+    setTimeout(() => {
+      wrongMsg.style.display = 'none';
+    }, 4000);
+  }
+  if(proceed==true){
+    const datos = {
+      name: nam,
+      email: mai,
+      phone: pho,
+      message: msg
+    };
+    
+    const body = {
+      item: datos
+    };
+
+    const fetchParams = {
+      method: 'POST',
+      headers: {
+        "Accept":'application/json',
+        "Content-Type":'application/json',
+        "X-API-Key": 'a0wwnrex_JeRhBybn5iFYziStv9d2M6Mchd2b4B4H'
+      },
+      body: JSON.stringify(body)
+    };
+ 
+    fetch(url, fetchParams)
+    .then((response) => response.json())
+    .then((data) => { 
+      console.log(data); 
+      allOk.style.display = 'block';
+      allOk.scrollIntoView();
+      setTimeout(() => {
+        allOk.style.display = 'none';
+      }, 4000);
+      document.getElementById("myForm").reset();
+    })
+    .catch(console.error);
+  }//IF
+
   
 });
 
-
+/*
 function validateFields() {
   const nam = document.getElementById('name').value;
   const mai = document.getElementById('email').value;
@@ -97,4 +169,4 @@ function sendFrom(nom,mai,pho,msg) {
     });
 }
 
-
+*/
