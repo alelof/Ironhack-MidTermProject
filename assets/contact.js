e08fece1dba4ea22902f3cbc821bb07b326da739
alelof <alejandra.lof@gmail.com>
2022-09-15 16:16:55 +0200

    FINISHED!
    Cleaning my code.
    Manual mail validation :)

--- assets/contact.js
+++ assets/contact.js
@@ -17,8 +17,8 @@ function(e){
   const wrongPhone = document.getElementById('wrongPhone');
   const wrongMsg = document.getElementById('wrongMsg');
   const allOk = document.getElementById('allOk');
+  const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   let proceed = true;
-//console.log(nam+mai+pho+msg);
 
   if (!nam || !mai || !pho || !msg){ //EmptyFields
       proceed = false;
@@ -44,6 +44,14 @@ function(e){
     setTimeout(() => {
       wrongMsg.style.display = 'none';
     }, 4000);
+  }
+  if(!mai.match(regex)){
+    proceed = false;
+    wrongMail.style.display = 'block';
+    wrongMail.scrollIntoView();
+    setTimeout(() => {
+      wrongMail.style.display = 'none';
+    }, 4000);    
   }
   if(proceed==true){
     const datos = {
@@ -78,95 +86,8 @@ function(e){
       }, 4000);
       document.getElementById("myForm").reset();
     })
-    .catch(console.error);
+    .catch(console.error); 
   }//IF
 
   
 });
-
-/*
-function validateFields() {
-  const nam = document.getElementById('name').value;
-  const mai = document.getElementById('email').value;
-  const pho = document.getElementById('phone').value;
-  const msg = document.getElementById('message').value;
-  const missingData = document.getElementById('missingData');
-  const wrongPhone = document.getElementById('wrongPhone');
-  const wrongMsg = document.getElementById('wrongMsg');
-  const allOk = document.getElementById('allOk');
-  let proceed = true;
-
-  let alertMsg = '¡ERROR!\nPlease verify the following:\n\n';
-  if (!nam && !mai && !pho && !msg){
-      proceed = false;
-      missingData.style.display = 'block';
-      missingData.scrollIntoView();
-      setTimeout(() => {
-        missingData.style.display = 'none';
-      }, 3000);      
-  }
-  else {
-    if(pho[0] != '+'){
-      proceed = false;
-      wrongPhone.style.display = 'block';
-      wrongPhone.scrollIntoView();  
-      setTimeout(() => {
-        wrongPhone.style.display = 'none';
-      }, 4000);
-      //alertMsg += "- Phone number should start with +\n";
-    }
-    if(msg.length > 50){
-      proceed = false;
-      wrongMsg.style.display = 'block';
-      wrongMsg.scrollIntoView();
-      setTimeout(() => {
-        wrongMsg.style.display = 'none';
-      }, 4000);
-    }
-  }
-
-  if(proceed==true){
-    allOk.style.display = 'block';
-    allOk.scrollIntoView();
-    document.getElementById("myForm").reset();
-
-    sendFrom(nam,mai,pho,msg);
-  }
-   
-}
-
-function sendFrom(nom,mai,pho,msg) {
-  alert("Hello from SendForm function");
-  const datos = {
-    name: nom,
-    email: mai,
-    phone: pho,
-    message: msg
-  };
-    
-  const body = {
-    item: datos
-  };
-
-  const fetchParams = {
-    method: 'POST',
-    headers: {
-      "Accept":'application/json',
-      "Content-Type":'application/json',
-      "X-API-Key": 'a0wwnrex_JeRhBybn5iFYziStv9d2M6Mchd2b4B4H'
-    },
-    body: JSON.stringify(body)
-  };
-
-  fecth(url, fetchParams)
-    .then(response =>{
-      if(response.ok) return response.JSON();
-
-    })
-    .then(json => {
-      console.log(json);
-      alert("wiiiiiiiiiii");
-    });
-}
-
-*/
